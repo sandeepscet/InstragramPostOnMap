7ae74e4d4dba33fd933f85762dd5ddee2f775913
sandeep <sandeep.scet@gmail.com>
2017-07-16 13:53:34 +0530

    formatted.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,200 +10,185 @@
                 <div class="panel-body">
                     <div id="my_view">
                         <div id="searchDiv">
-                        <form onsubmit="return false">
-                           <input type="text" name="instaUserName" v-model="instaUserName" value="katyperry" required/>
-                           <button v-on:click="searchInstaPosts" id="getPost">Get</button>
-                        </form>
+                            <form onsubmit="return false">
+                                <input type="text" name="instaUserName" v-model="instaUserName" value="katyperry" required/>
+                                <button v-on:click="searchInstaPosts" id="getPost">Get</button>
+                            </form>
                         </div>
-                     </div>
+                    </div>
                 </div>
 
                 <ul id="listPosts" style="display: none">
-                  <li v-for="post in posts">
-                    @{{ post.caption | truncate 50}}
-                    <button v-on:click="locateInstaPosts(post)">Locate</button>
-                    <button v-on:click="saveInstaPosts(post)">Save</button>
-                  </li>
+                    <li v-for="post in posts">
+                        @{{ post.caption | truncate 50}}
+                        <button v-on:click="locateInstaPosts(post)">Locate</button>
+                        <button v-on:click="saveInstaPosts(post)">Save</button>
+                    </li>
                 </ul>
 
                 <div id="postMapdata" style="display: none;">
                     <div id="map" style="margin: 5px;height: 500px;width: 500px"></div>
                 </div>
-    
+
             </div>
         </div>
     </div>
 </div>
 
-    <script>
-
-       function addMarkers(map, data) {
-        var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(data.location.lat, data.location.lng),
-          map: map
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-            var infowindow = new google.maps.InfoWindow();
-            var contentString = '<div data-postId="'+data.id+'" class="infoMarker"><strong>' + data.caption + '</strong><img width="'+data.thumbnail.width+'" height="'+data.thumbnail.height+'" src="'+data.thumbnail.url+'" /></div>';
-            infowindow.setContent(contentString);
-              infowindow.open(map, this);
-            });
-    }
-
-    function locatePostOnMap(post)
-    {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: { lat : post.location.lat, lng: post.location.lng },
-          zoom: 15
-        });
-        addMarkers(map, post);
-    }
-
-      function initMap(posts) {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: posts[0]['location']['lat'], lng: posts[0]['location']['lng']},
-          zoom: 15
-        });
-
-        for(var i =0;i< posts.length; i++)
-           { 
-                addMarkers(map, posts[i]);
-           }
-    }
-
-    </script>
-
-
-<script>
-
-    Vue.filter('truncate', function (text, stop, clamp) {
-        return text.slice(0, stop) + (stop < text.length ? clamp || '...' : '')
-    })
-
-    var myViewModel = new Vue({
-      el: '#my_view',
-      methods:
-        {
-            searchInstaPosts: function()
-            {   
-                getInstaPostData(this.$get('instaUserName') , function(posts){
-                        if(posts.length)
-                        {
-                            listPosts(posts);
-                            $('#postMapdata').show();
-                            
-                            var postWithLocation = [];
-                            for(var i =0;i<posts.length;i++)
-                            {
-                                if(posts[i].location)
-                                {
-                                    postWithLocation.push(posts[i]);    
-                                }
-                            }
-
-                            if(postWithLocation.length)
-                            {    
-                                renderGoogleMap(postWithLocation);
-                            }
-                            else
-                            {
-                                alert('no Post with location found');
-                            }
-                        }
-                        else
-                        {
-                            alert('Post for mentioned username not found');
-                        }
-                    });
-            }
-        }
+<script type="text/javascript">
+function addMarkers(map, data) {
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(data.location.lat, data.location.lng),
+        map: map
     });
 
-    function renderGoogleMap(posts)
-    {
-        var googleMapScriptUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyC2ibM65vFttmlKibb1yv8eUyfWKPa1Otk&libraries=places";
+    google.maps.event.addListener(marker, 'click', function() {
+        var infowindow = new google.maps.InfoWindow();
+        var contentString = '<div data-postId="' + data.id + '" class="infoMarker"><strong>' + data.caption + '</strong><img width="' + data.thumbnail.width + '" height="' + data.thumbnail.height + '" src="' + data.thumbnail.url + '" /></div>';
+        infowindow.setContent(contentString);
+        infowindow.open(map, this);
+    });
+}
 
-        loadScript(googleMapScriptUrl, function(){
-                initMap(posts);
-        });
+function locatePostOnMap(post) {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: post.location.lat,
+            lng: post.location.lng
+        },
+        zoom: 15
+    });
+    addMarkers(map, post);
+}
+
+function initMap(posts) {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: posts[0]['location']['lat'],
+            lng: posts[0]['location']['lng']
+        },
+        zoom: 15
+    });
+
+    for (var i = 0; i < posts.length; i++) {
+        addMarkers(map, posts[i]);
     }
+}
 
+Vue.filter('truncate', function(text, stop, clamp) {
+    return text.slice(0, stop) + (stop < text.length ? clamp || '...' : '')
+})
 
-    function getInstaPostData(instaUserName = '', callback)
-    {
-            $.ajax({
-                    url: '/home/getInstaPost',
-                    data: {instaUserName : instaUserName,"_token": "{{ csrf_token() }}",},
-                    method: 'GET'
-              }).then(function (posts) {
-                    callback(posts)
-                    
-              })
+var myViewModel = new Vue({
+    el: '#my_view',
+    methods: {
+        searchInstaPosts: function() {
+            getInstaPostData(this.$get('instaUserName'), function(posts) {
+                if (posts.length) {
+                    listPosts(posts);
+                    $('#postMapdata').show();
+
+                    var postWithLocation = [];
+                    for (var i = 0; i < posts.length; i++) {
+                        if (posts[i].location) {
+                            postWithLocation.push(posts[i]);
+                        }
+                    }
+
+                    if (postWithLocation.length) {
+                        renderGoogleMap(postWithLocation);
+                    } else {
+                        alert('no Post with location found');
+                    }
+                } else {
+                    alert('Post for mentioned username not found');
+                }
+            });
+        }
     }
+});
 
-     function saveInstaPostData(post, callback)
-    {
-            $.ajax({
-                    url: '/home/saveInstaPost',
-                    data: {post : post,"_token": "{{ csrf_token() }}",},
-                    method: 'POST'
-              }).then(function (posts) {
-                    callback(posts)
-                    
-              })
-    }
+function renderGoogleMap(posts) {
+    var googleMapScriptUrl = "https://maps.googleapis.com/maps/api/js?key=AIzaSyC2ibM65vFttmlKibb1yv8eUyfWKPa1Otk&libraries=places";
 
-    function listPosts(posts)
-    {
-        $('#listPosts').show();
-        var vueListPost = new Vue({
-          el: '#listPosts',
-          data: {
+    loadScript(googleMapScriptUrl, function() {
+        initMap(posts);
+    });
+}
+
+
+function getInstaPostData(instaUserName = '', callback) {
+    $.ajax({
+        url: '/home/getInstaPost',
+        data: {
+            instaUserName: instaUserName,
+            "_token": "{{ csrf_token() }}",
+        },
+        method: 'GET'
+    }).then(function(posts) {
+        callback(posts)
+
+    })
+}
+
+function saveInstaPostData(post, callback) {
+    $.ajax({
+        url: '/home/saveInstaPost',
+        data: {
+            post: post,
+            "_token": "{{ csrf_token() }}",
+        },
+        method: 'POST'
+    }).then(function(posts) {
+        callback(posts)
+
+    })
+}
+
+function listPosts(posts) {
+    $('#listPosts').show();
+    var vueListPost = new Vue({
+        el: '#listPosts',
+        data: {
             posts: posts
-          },
-          methods:{
-            saveInstaPosts: function(post){  
-                    saveInstaPostData(post, function(response){
-                        alert('Post saved');
-                    })
-                },
-            locateInstaPosts: function(post){   
-                    if(post.location)
-                    {    
-                        locatePostOnMap(post);
-                    }
-                    else
-                    {
-                        alert('No location found with this post');
-                    }
+        },
+        methods: {
+            saveInstaPosts: function(post) {
+                saveInstaPostData(post, function(response) {
+                    alert('Post saved');
+                })
+            },
+            locateInstaPosts: function(post) {
+                if (post.location) {
+                    locatePostOnMap(post);
+                } else {
+                    alert('No location found with this post');
                 }
             }
-        })
-    }
-
-    
-
-    function loadScript(src, callback)
-    {
-      var s,
-          r,
-          t;
-      r = false;
-      s = document.createElement('script');
-      s.type = 'text/javascript';
-      s.src = src;
-      s.onload = s.onreadystatechange = function() {
-        //console.log( this.readyState ); //uncomment this line to see which ready states are called.
-        if ( !r && (!this.readyState || this.readyState == 'complete') )
-        {
-          r = true;
-          callback();
         }
-      };
-      t = document.getElementsByTagName('script')[0];
-      t.parentNode.insertBefore(s, t);
-    }
+    })
+}
+
+
+
+function loadScript(src, callback) {
+    var s,
+        r,
+        t;
+    r = false;
+    s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = src;
+    s.onload = s.onreadystatechange = function() {
+        //console.log( this.readyState ); //uncomment this line to see which ready states are called.
+        if (!r && (!this.readyState || this.readyState == 'complete')) {
+            r = true;
+            callback();
+        }
+    };
+    t = document.getElementsByTagName('script')[0];
+    t.parentNode.insertBefore(s, t);
+}
 </script>
 
 
--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -10,105 +10,104 @@
                 <div class="panel-body">
                     <div id="my_view">
 
-                    <form class="form-horizontal" onsubmit="return false;">
-                      <fieldset>
+                        <form class="form-horizontal" onsubmit="return false;">
+                            <fieldset>
 
-                      <div class="form-group">
-                        <label class="col-md-4 control-label" for="searchTerm">Search</label>  
-                        <div class="col-md-4">
-                        <input id="searchTerm" name="searchTerm" v-model="searchTerm" type="text" placeholder="Search in caption" class="form-control input-md">
-                        </div>
-                      </div>
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="searchTerm">Search</label>
+                                    <div class="col-md-4">
+                                        <input id="searchTerm" name="searchTerm" v-model="searchTerm" type="text" placeholder="Search in caption" class="form-control input-md">
+                                    </div>
+                                </div>
 
-                      <div class="form-group">
-                        <label class="col-md-4 control-label" for="sortBy">Search radius</label>
-                        <div class="col-md-4">
-                          <select id="sortBy" name="radius" class="form-control" v-model="sortOrder">
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="sortBy">Sort order</label>
+                                    <div class="col-md-4">
+                                        <select id="sortBy" name="radius" class="form-control" v-model="sortOrder">
                             <option value="asc">Ascending</option>
                             <option value="desc">Descending</option>
                           </select>
-                        </div>
-                      </div>
+                                    </div>
+                                </div>
 
-                      <div class="form-group">
-                      <label class="col-md-4 control-label" for="submit"></label>
-                      <div class="col-md-4">
-                        <button id="submit" name="submit" class="btn btn-primary"  v-on:click="searchPosts">Search</button>
-                      </div>
-                    </div>
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label" for="submit"></label>
+                                    <div class="col-md-4">
+                                        <button id="submit" name="submit" class="btn btn-primary" v-on:click="searchPosts">Search</button>
+                                    </div>
+                                </div>
 
-                      </fieldset>
-                    </form>
+                            </fieldset>
+                        </form>
 
-                    <div id="gallaryBlock">
+                        <div id="gallaryBlock">
                             <div class="row">
-                              <div class="gallery col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                  <h1 class="gallery-title">Gallery</h1>
-                              </div>
+                                <div class="gallery col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                    <h1 class="gallery-title">Gallery</h1>
+                                </div>
 
-                              <h1 v-if="posts.length == 0">No Post found.</h1>
-                              <div v-for="post in posts" class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter sprinkle" style="margin-bottom: 30px;">
-                                  <img v-bind:src=post.standard_resolution.url class="img-responsive"  height="@{{ post.standard_resolution.height" width="@{{ post.standard_resolution.width" alt="@{{ post.caption  | truncate 50}}">
-                                      <p>@{{ post.caption  | truncate 50}}</p>
-                              </div>
-                              
-                          </div>
+                                <h1 v-if="posts.length == 0">No Post found.</h1>
+                                <div v-for="post in posts" class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter sprinkle" style="margin-bottom: 30px;">
+                                    <img v-bind:src=post.standard_resolution.url class="img-responsive" height="@{{ post.standard_resolution.height" width="@{{ post.standard_resolution.width" alt="@{{ post.caption  | truncate 50}}">
+                                    <p>@{{ post.caption | truncate 50}}</p>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 
 <script type="text/javascript">
 
-Vue.filter('truncate', function (text, stop, clamp) {
-        return text.slice(0, stop) + (stop < text.length ? clamp || '...' : '')
-    })
+Vue.filter('truncate', function(text, stop, clamp) {
+    return text.slice(0, stop) + (stop < text.length ? clamp || '...' : '')
+})
 
 var myModel = {
-  posts: [],
-  sortOrder : 'desc'
-  };
+    posts: [],
+    sortOrder: 'asc'
+};
 
-  var myViewModel = new Vue({
+var myViewModel = new Vue({
     el: '#my_view',
     data: myModel,
-      ready : function()
-        {
-          this.fetchInitialPosts();
-    
+    ready: function() {
+        this.fetchInitialPosts();
+
+    },
+    methods: {
+        fetchInitialPosts: function() {
+            var $this = this;
+            getPostData("", 'asc', function(posts) {
+                $this.$set('posts', posts);
+            });
         },
-         methods:
-        {
-          fetchInitialPosts: function()
-            {
-              var $this = this;
-              getPostData("", 'asc' , function(posts){
-                  $this.$set('posts', posts);
-                });
-         } ,
-        searchPosts: function()
-            { 
-              var $this = this;
-              getPostData(this.$get('searchTerm'), this.$get('sortOrder') , function(posts){
-                 $this.$set('posts', posts);
-                });
+        searchPosts: function() {
+            var $this = this;
+            getPostData(this.$get('searchTerm'), this.$get('sortOrder'), function(posts) {
+                $this.$set('posts', posts);
+            });
         }
     }
-  });
+});
 
-  function getPostData(searchTerm = '', sortOrder = 'true', callback)
-  {
-      $.ajax({
-               url: '/home/listInstaPost',
-                data: {searchTerm : searchTerm , sortOrder : sortOrder ,"_token": "{{ csrf_token() }}",},
-          }).then(function (posts) {
-              callback(posts)
-              
-          })
-  }
+function getPostData(searchTerm = '', sortOrder = 'true', callback) {
+    $.ajax({
+        url: '/home/listInstaPost',
+        data: {
+            searchTerm: searchTerm,
+            sortOrder: sortOrder,
+            "_token": "{{ csrf_token() }}",
+        },
+    }).then(function(posts) {
+        callback(posts)
+
+    })
+}
 </script>
 
 @endsection
